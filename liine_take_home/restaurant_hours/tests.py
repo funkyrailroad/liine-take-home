3c78fa40acc00a025b749c33ac777229a5cb4c22
Jake Atwell <jatwell@alexinc.com>
2022-11-16 20:04:44 -0500

    split_days_and_hours_mention function

--- liine_take_home/restaurant_hours/tests.py
+++ liine_take_home/restaurant_hours/tests.py
@@ -1,22 +1,36 @@
 # Create your tests here.
-from django.conf import settings
+from pprint import pprint
+
 from django.test import TestCase
 
 import restaurant_hours.util as u
 
+
 class DataCleaningTests(TestCase):
+    def check_split_opening_days_and_hours(
+        self, days_and_hours_mention, days_gt, hours_gt
+    ):
+        days_mention, hours_mention = u.split_days_and_hours_mention(
+            days_and_hours_mention
+        )
+        self.assertEqual(days_mention, days_gt)
+        self.assertEqual(hours_mention, hours_gt)
 
-    # def test_1(self):
-    #     import csv
-    #     BASE_DIR = settings.BASE_DIR
+    def test_split_opening_days_and_hours(self):
+        self.check_split_opening_days_and_hours(
+            "Sun 12 pm - 10 pm",
+            "Sun",
+            "12 pm - 10 pm",
+        )
 
-    #     with open(f"{BASE_DIR}/restaurant_hours/restaurants.csv") as file:
-    #         reader = csv.reader(file)
-    #         for row in reader:
-    #             print(", ".join(row))
+        self.check_split_opening_days_and_hours(
+            "Sat-Sun 5 pm - 10 pm",
+            "Sat-Sun",
+            "5 pm - 10 pm",
+        )
 
-
-    def test_normalize_hours_mention(self):
-        mention = "Sat-Sun 5 pm - 10 pm"
-        mentions = u.get_all_hours_mentions()
-        print(mentions)
+        self.check_split_opening_days_and_hours(
+            "Mon-Thu, Sun 11:30 am - 10 pm",
+            "Mon-Thu, Sun",
+            "11:30 am - 10 pm",
+        )
--- liine_take_home/restaurant_hours/util.py
+++ liine_take_home/restaurant_hours/util.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 from django.conf import settings
 
@@ -17,20 +18,38 @@ def clean_hours_field():
     pass
 
 
-def split_hours_field():
-    pass
+def split_hours_field(hours_field):
+    """Split and remove whitespace in hours field"""
+    return [mention.strip() for mention in hours_field.split("/")]
 
 
 def normalize_hours_mention():
     pass
 
 
-def get_all_hours_mentions():
+def get_all_days_and_hours_mentions():
     mentions = []
 
     with open(f"{BASE_DIR}/restaurant_hours/restaurants.csv") as file:
         reader = csv.reader(file)
+        file.readline()  # skip first row
         for row in reader:
-            print(row[1])
+            hours_field = row[1]
+            mentions.extend(split_hours_field(hours_field))
 
     return mentions
+
+
+def split_days_and_hours_mention(mention):
+    """Opening days are the first part of the string and can potentially be:
+
+    - a single day
+    - multiple consecutive days
+    - multiple non-consecutive days
+    - or any combination thereof
+
+    A reliable split seems to be the first number in the string
+    """
+    split_index = re.search(r"\d", mention).start()
+    days, hours = mention[:split_index], mention[split_index:]
+    return days.strip(), hours.strip()
