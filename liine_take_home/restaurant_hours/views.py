5d93b85c27cdc56e7966a4cc1d09ff4edf853c40
Jake Atwell <jatwell@alexinc.com>
2022-11-19 22:11:41 -0500

    Get an endpoint operational

--- liine_take_home/restaurant_hours/views.py
+++ liine_take_home/restaurant_hours/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class TestView(APIView):
+    def get(self, request):
+        return Response("Here's a response.")
