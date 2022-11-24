744b93ef030281454af2561341960be507635af1
Jake Atwell <jatwell@alexinc.com>
2022-11-24 18:08:58 -0500

    Make model for weekly opening hours data schema

--- liine_take_home/restaurant_hours/models.py
+++ liine_take_home/restaurant_hours/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class WeeklyOpeningHours(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+    days_of_the_week = [
+        ("Mon", "Monday"),
+        ("Tues", "Tuesday"),
+        ("Wed", "Wednesday"),
+        ("Thu", "Thursday"),
+        ("Fri", "Friday"),
+        ("Sat", "Saturday"),
+        ("Sun", "Sunday"),
+    ]
+
+    day = models.CharField(max_length=4, choices=days_of_the_week, blank=False)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
