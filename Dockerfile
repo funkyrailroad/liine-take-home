5883d356f2cf0166ef5081f3439466f864efb583
Jake Atwell <jatwell@alexinc.com>
2022-11-24 13:25:37 -0500

    Switch db to postgres

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.10
 
 RUN pip install django~=4.0 djangorestframework
 RUN pip install pdbpp
+RUN pip install psycopg2
 
 ENTRYPOINT ["bash"]
--- liine_take_home/liine_take_home/settings.py
+++ liine_take_home/liine_take_home/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'liine_take_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'liine',
+        'USER': 'postgres',
+        'PASSWORD': 'takehome',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
